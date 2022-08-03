03a36809d293486347f742ff292e9215b6bd0a7e
Andriy Pustovit <andriy.pustovit@mail.ru>
2022-08-03 07:41:26 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,75 +1,33 @@
-// const throttle = require('lodash.throttle');
+const throttle = require('lodash.throttle');
+let formData = {};
 
-// const formEl = document.querySelector('form');
-
-// formEl.addEventListener('submit', notOpenNewPage);
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// function notOpenNewPage(evt) {
-//   evt.preventDefault();
-
-//   console.log('last');
-//   evt.target.reset();
-
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function textreal() {
-//   const saveMessage = localStorage.getItem(STORAGE_KEY);
-//   if (saveMessage) {
-//     formEl.value = saveMessage;
-//   }
-// }
-
-import throttle from 'lodash.throttle';
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form  textarea'),
-  input: document.querySelector('.feedback-form  input'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(savingData, 500));
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', notOpenNewPage);
+formEl.addEventListener('input', throttle(onFormInput, 500));
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
+const localData = localStorage.getItem(STORAGE_KEY);
 
-function savingData(event) {
-  formData[event.target.name] = event.target.value;
-  console.log(formData);
+if (localData) {
+  formData = JSON.parse(localData);
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-addEventListener('DOMContentLoaded', populateTextarea);
-
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-  if (savedMessage) {
-    try {
-      objectMessage = JSON.parse(savedMessage);
-
-      refs.input.value = objectMessage.email;
-      refs.textarea.value = objectMessage.message;
-    } catch (error) {
-      console.log(error.message);
-    }
-  } else {
-    refs.input.value = '';
-    refs.textarea.value = '';
+  for (let key in formData) {
+    formEl.elements[key].value = formData[key];
   }
 }
 
-function onFormSubmit(evt) {
+function notOpenNewPage(evt) {
   evt.preventDefault();
 
-  // clearing a form when submit
-  evt.currentTarget.reset();
+  evt.target.reset();
 
-  // removing storageKey info from the localStorage
   localStorage.removeItem(STORAGE_KEY);
+
+  console.log(formData);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
